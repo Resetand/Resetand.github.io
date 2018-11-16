0bd3e1d6b1a2d0c98c7fbab8767dfa4e6774366e
Resetand <machehin00@gmail.com>
2018-11-16 14:43:35 +0300

    edit animation in Bear

--- Bear/js/script.js
+++ Bear/js/script.js
@@ -2,17 +2,21 @@ jQuery(document).ready(function($) {
 	$("#button1").on('click', function(event) {
 		event.preventDefault();
 		$("#about-hiden-text").slideToggle();
+		
 	});
 	$("#bt-more-1").on('click', function(event) {
 		event.preventDefault();
+		$("#bt-more-1").fadeOut(0);
 		$("#hiden-text-1").fadeIn(1000);
 	});
 	$("#bt-more-2").on('click', function(event) {
 		event.preventDefault();
+		$("#bt-more-2").fadeOut(0);
 		$("#hiden-text-2").fadeIn(1000);
 	});
 	$("#bt-more-3").on('click', function(event) {
 		event.preventDefault();
+		$("#bt-more-3").fadeOut(0);
 		$("#hiden-text-3").fadeIn(1000);
 	});
 	$("#show-btn").on("click", function(event) {
