540a96b1bf7b173b3252df24867433c979e265f9
Kirill <machehin00@gmail.com>
2018-12-21 18:17:18 +0300

    Revert "delete"
    This reverts commit f8567af367c67c8ea96d8890141ecc984559caa7.

---
+++ GULP-START/gulp/paths/tasks.js
@@ -0,0 +1,9 @@
+module.exports = [
+    './gulp/tasks/watch',
+    './gulp/tasks/clean',
+    './gulp/tasks/serve',
+    './gulp/tasks/svg',
+    './gulp/tasks/img',
+    './gulp/tasks/html',
+    './gulp/tasks/fonts',
+];
