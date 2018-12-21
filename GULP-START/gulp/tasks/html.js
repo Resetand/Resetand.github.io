359c82c82018716b69b9be5feab54403d6064f2c
Kirill <machehin00@gmail.com>
2018-12-21 18:17:23 +0300

    Revert "add gulp build"
    This reverts commit c02f4aa90e77f0e0d742ff892f4934f80ff722e8.

--- GULP-START/gulp/tasks/html.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-    $.gulp.task('html', ()=>  {
-        return $.gulp.src('./dev/*.html')
-            .pipe($.gulp.dest('./build/'))
-            .on('end', $.browserSync.reload);
-    });
-};
