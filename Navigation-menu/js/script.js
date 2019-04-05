44d5a3416d9cf1891c58e97e55134054cc37c8ba
Kirill <machehin00@gmail.com>
2019-04-05 17:25:30 +0300

    del

--- Navigation-menu/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".hamburger-box").on("click", function () {
-        $(".hamburger-inner").toggleClass('hamburger-click');
-        $(this).toggleClass("btn-active");
-        $(".hidden-nav").toggleClass("active-nav-menu")
-    });
-  })
--- metro/metro_map/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetroMapConfig(AppConfig):
-    name = 'metro_map'
