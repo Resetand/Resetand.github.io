31a868b3c470af9e9086b93bb88bb54d5c6b4817
Kirill <machehin00@gmail.com>
2019-05-16 17:41:52 +0300

    add

---
+++ bear/index.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">   <!--  LANG -->
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap, gonts, normalize CSS -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800|Raleway:100,200,300,400,400i,500,500i,600,700" rel="stylesheet">
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
+    <!-- live reload -->
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    <title>Bear</title>
+  </head>
+  <body>
+  <div class="MAINWRAP">
+  <div class="container">    
+
+    <header>
+      <div class="row">
+        <div class="card col-lg-7 mx-auto">
+            <div class="card-composition">
+              <div class="card-composition-small-headline">Lorem ipsum</div>
+              <div class="card-composition-line"></div>
+              <div class="card-composition-big-headline">Travel</div> 
+              <div class="card-composition-text">THE INSPIRATIONAL ADVENTURES AROUND THE WORLD</div>
+            </div>  
+        </div>
+      </div>
+    </header>
+    <section id="ABOUT">
+      <div class="row"> 
+        <div class="about-composition col-12 col-md-6 col-lg-5 order-md-1 order-2">
+          <div class="brown-headline">About me</div>
+          <div class="brown-line"></div>
+          <div class="textblock text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident<span id="about-hiden-text">, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</span></p>
+          </div>
+          <a href="#" id="button1"class="button">READ MORE</a>
+        </div>
+        <div class="about-photo col-12 col-md-6 col-lg-5 offset-lg-1 order-md-2 order-1"></div>
+      </div>
+    </section>
+
+
+    <section id="LA">
+      <div class="LA-main-colmposition"></div>
+        <div class="row">
+          <div class="LA-headline col-sm-5 mx-auto">
+            <div class="brown-headline">Lastest articles</div>
+            <div class="brown-line"></div>
+          </div>
+        </div>
+        <div class="row LA-items">
+           <div class="LA-item col-xl-4 col-sm-10 mx-auto col-12">
+            <div class="photo-shadow"><img src="img/Prostokąt 7 kopia 2.png" alt=""></div>
+            <div class="LA-brown-headline">Lorem Ipsum Dolor</div>
+            <div class="brown-line"></div>
+            <div class="text">
+              <p>Lorem ipsum dolor sit amet, consectetur pisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span id="hiden-text-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam recusandae obcaecati accusantium nesciunt iusto voluptas temporibus minus, error minima quasi.Nisi ab, enim, mollitia vel eum voluptatum possimus at dignissimos necessitatibus quis repudiandae. Deleniti facilis velit tempora ex ad culpa. Odit, temporibus.</span></p>
+              <a href="#" id="bt-more-1">Read more <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+            </div>
+          </div>
+          <div class="LA-item LA-items-2 col-xl-4 col-sm-10 mx-auto col-12">
+            <div class="photo-shadow"><img src="img/Clip.png"></div>
+            <div class="LA-brown-headline">Lorem Ipsum Dolor</div>
+            <div class="brown-line"></div>
+            <div class="text">
+              <p>Lorem ipsum dolor sit amet, consectetur pisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span id="hiden-text-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam recusandae obcaecati accusantium nesciunt iusto voluptas temporibus minus, error minima quasi.Nisi ab, enim, mollitia vel eum voluptatum possimus at dignissimos necessitatibus quis repudiandae. Deleniti facilis velit tempora ex ad culpa. Odit, temporibus.</span></p>
+              <a href="#" id="bt-more-2">Read more <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+            </div>
+          </div>
+          <div class="LA-item col-xl-4 col-sm-10 mx-auto col-12">
+            <div class="photo-shadow"><img src="img/Clip2.png" alt=""></div>
+            <div class="LA-brown-headline">Lorem Ipsum Dolor</div>
+            <div class="brown-line"></div>
+            <div class="text">
+              <p>Lorem ipsum dolor sit amet, consectetur pisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span id="hiden-text-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam recusandae obcaecati accusantium nesciunt iusto voluptas temporibus minus, error minima quasi.Nisi ab, enim, mollitia vel eum voluptatum possimus at dignissimos necessitatibus quis repudiandae. Deleniti facilis velit tempora ex ad culpa. Odit, temporibus.</span></p>
+              <a href="#" id="bt-more-3">Read more <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+     
+    </section>
+    <section id="GALLERY">
+        <div class="gallery-title">
+          <div class="brown-headline">Gallery</div>
+          <div class="brown-line"></div>
+        </div>
+        <div class="gallery-items">
+          <div class="row gallery-row-1 align-items-end">
+            <div class="col-lg-4 col-sm-6 "><img src="/img/gallery/pexels-photo-1404918.jpeg" width="370" height="349" alt=""></div>
+            <div class="col-lg-4 col-sm-6"><img src="/img/gallery/pexels-photo-1574184.jpeg" width="411" height="349" alt=""></div>
+            <div class="col-lg-4 col-sm-6" ><img src="/img/gallery/pexels-photo-1574843.jpeg" width="402" height="212" alt=""></div>
+          </div>
+          <div class="row gallery-row-2 aling-items-end">
+            <div class="col-lg-4 col-sm-5 offset-lg-0 offset-sm-7"><img src="/img/gallery/pexels-photo-1553961.jpeg" width="402" height="564" alt=""></div>
+            <div class="col-lg-8"><img src="/img/gallery/pexels-photo-988914.jpeg" width="853" height="349" alt=""></div>
+          </div>
+          <div class="row gallery-row-3 aling-items-end">
+            <div class="col-lg-4 offset-lg-4 col-sm-6"><img src="/img/gallery/pexels-photo-923345.jpeg" width="394" height="349" alt=""></div>
+            <div class="col-lg-4"><img src="/img/gallery/pexels-photo.jpg" width="402" height="231" alt=""></div>
+          </div>
+
+          <div class="row gallery-hidden-row-1">
+            <div class="col-lg-4 col-sm-6"><img src="/img/gallery/hidden-rows/pexels-photo-203629.jpeg" alt=""> </div>
+            <div class="col-lg-4 col-sm-6"><img src="/img/gallery/hidden-rows/pexels-photo-747964.jpeg"></div>
+            <div class="col-lg-4 col-sm-6 offset-sm-6 offset-lg-0"><img src="/img/gallery/hidden-rows/pexels-photo-347133.jpeg" alt=""></div>
+          </div>
+          <div class="row gallery-hidden-row-2">
+            <div class="col-lg-4 col-sm-6"><img src="/img/gallery/hidden-rows/pexels-photo.jpg"></div>
+            <div class="col-lg-8 offset-lg-0"><img src="/img/gallery/hidden-rows/pexels-photo-259526.jpeg" alt=""></div>
+          </div>
+          <div class="show-more-btn d-flex justify-content-center">
+            <a href="#" class="button" id="show-btn">SHOW MORE</a> 
+          </div>
+        </div>
+    </section>
+    <section id="CONTACT">
+      <div class="row">
+        <div class="contact-photo col-md-6"></div>
+        <div class="col-md-6 col-12">
+          <div class="contact-composition">
+            <div class="brown-headline">Contact</div>
+            <div class="brown-line"></div>
+            <div class="inpust-composition">
+              <div class="name-and-email">
+                <input type="text" class="small-input name" placeholder="Name">
+                <input type="text=" class= "small-input email" placeholder="Email">
+              </div>
+              <textarea class="massage-input" placeholder="Message"></textarea>
+              
+            </div>
+            <a href="#" class="button">SEND MESSAGE</a>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+    <footer>
+        <a href="#"><div class="social-icon"><i class="fa fa-facebook" aria-hidden="true"></i></div></a>
+        <a href="#"><div class="social-icon"><i class="fa fa-twitter" aria-hidden="true"></i></div></a>
+        <a href="#"><div class="social-icon"><i class="fa fa-instagram" aria-hidden="true"></i></div></a>
+        <a href="#"><div class="social-icon"><i class="fa fa-pinterest" aria-hidden="true"></i></div></a>
+      </footer>
+		
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  	
+    <!-- My scripts -->
+    <script type="text/javascript" src="js/script.js"></script>
+  </div>
+  </body>
+</html>
